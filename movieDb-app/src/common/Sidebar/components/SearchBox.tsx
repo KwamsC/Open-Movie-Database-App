3b8df70e21a12fc319ae5219b2414bebcb98b2e3
Kwame Carr <kwamsc91@gmail.com>
2025-01-30 00:02:27 +0100

    Update search box placeholder text for brevity

--- movieDb-app/src/common/Sidebar/components/SearchBox.tsx
+++ movieDb-app/src/common/Sidebar/components/SearchBox.tsx
@@ -48,7 +48,7 @@ const SearchBox = ({
           value={filters.title}
           onChange={onChange}
           onKeyDown={onKeyDown}
-          placeholder="Search movies..."
+          placeholder="Search..."
           className="w-full px-4 py-2 min-h-[44px] rounded-xl bg-black/20 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
